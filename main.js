73fb6684e90d58fd568a367b67b168b5e661b7da
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-03-23 11:00:42 -0500

    made listener for sort student button

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+const students = [];
+const houses = ["Ravenclaw", "Hufflepuff", "Gryffindor", "Slytherin"];
 
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 };
-
 
 
 const tronBuilder = () => {
@@ -13,10 +14,12 @@ const tronBuilder = () => {
         domString +=    `<p class="lead">Thestral dirigible plums, Viktor Krum hexed memory charm Animagus Invisibility Cloak three-headed Dog. Half-Blood Prince Invisibility Cloak cauldron cakes, hiya Harry! Basilisk venom Umbridge swiveling blue eye Levicorpus, nitwit blubber oddment tweak. Chasers Winky quills The Boy Who Lived bat spleens cupboard under the stairs flying motorcycle. Sirius Black Holyhead Harpies, you’ve got dirt on your nose. Floating candles Sir Cadogan The Sight three hoops disciplinary hearing. Grindlewald pig’s tail Sorcerer's Stone biting teacup. Side-along dragon-scale suits Filch 20 points, Mr. Potter.</p>`;
         domString +=    `<hr class="my-4">`;
         domString +=    `<p>Thestral dirigible plums, Viktor Krum hexed memory charm Animagus Invisibility Cloak three-headed Dog.</p>`;
-        domString +=    `<a class="btn btn-primary btn-lg" id="sort-button" href="#" role="button">Let's Start Sorting!</a>`;
+        domString +=    `<a class="btn btn-primary btn-lg" id="started-button" href="#" role="button">Let's Start Sorting!</a>`;
         domString += `</div>`;
 
         printToDom("jumbotron", domString);
+        
+
     };
 
 const buildForm = () => {
@@ -27,15 +30,52 @@ const buildForm = () => {
         domString +=        `<label for="exampleInputName1">Student</label>`;
         domString +=        `<input type="name" class="form-control" id="exampleInputName1" aria-describedby="nameHelp" placeholder="Neville Longbottom">`;
         domString +=    `</div>`;
-        domString +=    `<button type="submit" class="btn btn-primary">Sort!</button>`;
+        domString +=    `<button type="submit" class="btn btn-primary" id="sort-button">Sort!</button>`;
         domString +=  `</form>`;
 
         printToDom("sort-form", domString);
+        document.getElementById("sort-button").addEventListener("click", addStudent);
     };
  
+const getHouse = () => {
+   
+   //The gitHouse function will contain an array with 4 houses called houses.
+
+   let randomNumber = Math.floor(Math.random() * 4);
+   // Math.floor((Math.random() * 10) + 1);
+
+   let houseIndex = randomNumber;
+    // -->then getHouse will generate a random number and store that number in a variable called houseIndex
+
+   return houses[houseIndex];
+
+   
+    // getHouse will then return houses[houseIndex]
+
+};
+
+const addStudent = () => {
+    // -->addStudent will take a name from the name input field and getHouse().
+    // (So instead of name as the parameter, shouldn't it take "exampleInputName1"? because
+    //that's the id for the name input field.)
+
+    //Do something with name and house
+    //newName = `${name} ${house}`;
+   // return newName;
+    let studentNameInput = document.getElementById("exampleInputName1").value;
+    console.log(studentNameInput);
+
+    let studentHouse = getHouse();
+    console.log(studentHouse);
+};
+
+
 
 const buttonEvents = () => {
-    document.getElementById("sort-button").addEventListener("click", buildForm);
+    document.getElementById("started-button").addEventListener("click", buildForm);
+    
+    //Make an event listener for the click on the sort student button.
+    //The listener will call the addStudent function. 
 
 };
 
@@ -43,6 +83,8 @@ const init = () => {
     
     tronBuilder();
     buttonEvents();
+ 
+   
    
 
 };
